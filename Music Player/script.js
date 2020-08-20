2ed8a1d5c0dad5b49cef03964cd39da6420b4a67
Your Name <you@example.com>
2020-08-20 13:36:49 +1000

    add some more projects

---
+++ Music Player/script.js
@@ -0,0 +1,93 @@
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const preBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+//Song titles
+const songs = ["hey", "summer", "ukulele"];
+
+//keep track of song
+let songIndex = songs.length - 1;
+
+//Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+//Update song Details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+//play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  audio.play();
+}
+//pause song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  audio.pause();
+}
+//previous song
+function preSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+//next song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+//Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+//set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+//event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+//Change song
+preBtn.addEventListener("click", preSong);
+nextBtn.addEventListener("click", nextSong);
+
+//Time/song update
+audio.addEventListener("timeupdate", updateProgress);
+
+//progress bar update
+progressContainer.addEventListener("click", setProgress);
+
+//song ends
+audio.addEventListener("ended", nextSong);
